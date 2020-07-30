e19eda545c3b20fdf2183ea9747cbdc1af0dff0f
Brandon Lu <brandonbl2021@gmail.com>
2020-07-30 13:37:55 -0400

    setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+//Components
+import MainPage from './components/Main_Page/MainPage';
+import BusinessNavBar from './components/BusinessNavBar';
+
+//Ant Design
+import { Layout } from 'antd';
+const { Content } = Layout;
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    )
+  }
 }
-
 export default App;
--- src/components/Main_Page/MainPage.js
+++ src/components/Main_Page/MainPage.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { Layout } from 'antd';
+import BusinessNavBar from '../BusinessNavBar';
+const PageContainer = styled.div`
+  display: flex;
+  width: 70%;
+  padding-left: 5%;
+  padding-right: 5%;
+  justifycontent: center;
+  background-color: white;
+  border-radius: 10px;
+`;
+
 class MainPage extends React.Component {
     render() {
-        return (
-            <div>
+        return (<div>
 
-            </div>
-        )
+        </div>)
     }
+
+
 }
 export default MainPage;
