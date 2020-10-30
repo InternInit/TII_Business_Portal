3753fb651830792bc991fd2012d6dfa10643e565
JosephZhang-LDL <20ZhangJ@nsboroschools.net>
2020-10-30 18:35:53 -0400

    Adjusted main page for responsive resizing

--- src/components/Main_Page/MainPage.js
+++ src/components/Main_Page/MainPage.js
@@ -32,7 +32,7 @@ class MainPage extends React.Component {
 
         <div className="dashboard-container px-4 py-2">
           <AntRow gutter={[32, 16]}>
-            <AntCol lg={16}>
+            <AntCol xs={24} sm={{span: 24, order: 1}} lg={16}>
               <Header className="twentyFont mb-point-5"> Listings</Header>
               {listings.slice(0, 5).map((post) => (
                 <PageListings
@@ -43,9 +43,24 @@ class MainPage extends React.Component {
                 />
               ))}
             </AntCol>
-            <AntCol lg={8}>
+            <AntCol xs={24} sm={{span: 12, order: 2}} lg={8}>
               <Header className="twentyFont mb-point-5">
                 Incoming Applicants
+              </Header>
+              {candidates
+                .filter((candidate) => candidate.status === "Pending")
+                .map((student) => (
+                  <StudentCard
+                    firstName={student.info["First Name"]}
+                    lastName={student.info["Last Name"]}
+                    age={" (" + student.info["Age"] + ")"}
+                    avatar={`https://tii-intern-media.s3.amazonaws.com/${student.studentId}/profile_picture`}
+                  />
+                ))}
+            </AntCol>
+            <AntCol xs={24} sm={{ span: 12, order: 2 }} lg={0}>
+              <Header className="twentyFont mb-point-5">
+                To be Interviewed
               </Header>
               {candidates
                 .filter((candidate) => candidate.status === "Pending")
@@ -61,11 +76,11 @@ class MainPage extends React.Component {
           </AntRow>
 
           <AntRow gutter={[32, 16]}>
-            <AntCol lg={16}>
+            <AntCol xs={24} sm={24} lg={16}>
               <Header className="twentyFont mb-point-5">Current Interns</Header>
               <PageFeedback />
             </AntCol>
-            <AntCol lg={8}>
+            <AntCol xs={0} lg={8}>
               <Header className="twentyFont mb-point-5">
                 To be Interviewed
               </Header>
