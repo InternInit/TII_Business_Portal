1574111034ebdc238ddd942a2294ecd9968624bf
JosephZhang-LDL <20ZhangJ@nsboroschools.net>
2020-12-16 23:43:29 -0500

    Intermediate design

--- src/components/Candidates/DraggingCard.jsx
+++ src/components/Candidates/DraggingCard.jsx
@@ -7,71 +7,66 @@ import {
   Header,
   Caption,
 } from "../Styled/FundamentalComponents.jsx";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 class DraggingCard extends React.Component {
   render() {
     let { name, date, city, stateLocation, position, avatar, id } = this.props;
     return (
       <TabContainer className="py-1-5 px-2 responsive-tab-container">
+        <Caption className="twelveFont dragging-card-date" light right>
+          Applied on <br /> {new Date(date[0]).getMonth()}/
+          {new Date(date[0]).getDate()}/{new Date(date[0]).getFullYear()}
+        </Caption>
         {/**
          *
          * Row Containing Name + Avatar
          *
          */}
-        <AntRow justify="center" align="middle">
-          <Avatar src={avatar} size={64} icon={<UserOutlined />} />
+        <AntRow justify="start" align="middle">
+          <AntCol>
+            <Avatar src={avatar} size={40} icon={<UserOutlined />} />
+          </AntCol>
+          <AntCol offset={1}>
+            <AntRow>
+              <Header className="twentyFont" bolded>
+                {name}
+              </Header>
+            </AntRow>
+            <AntRow>
+              <Caption
+                className="fourteenFont"
+                style={{ marginTop: "-5px" }}
+                light
+                thin
+                left
+              >
+                {position}
+              </Caption>
+            </AntRow>
+          </AntCol>
         </AntRow>
-        <AntRow>
-          <Header className="twentyFont mt-point-25" bolded>
-            {name}
-          </Header>
-        </AntRow>
-        <AntRow>
-          <Caption
-            className="fourteenFont"
-            style={{ marginTop: "-5px" }}
-            light
-            thin
-            left
-          >
-            {city}
-          </Caption>
-        </AntRow>
-        <AntRow className="mt-point-5">
-          <AntCol className="mr-point-25">
-            <Caption className="sixteenFont" light>
-              Availability:
+
+        <AntRow className="mt-1 mb-1-5">
+          <AntCol>
+            <Caption className="fourteenFont mr-point-25" light>
+              Location:{" "}
             </Caption>
           </AntCol>
           <AntCol>
-            <Caption className="sixteenFont">
-              {new Date(date[0]).getMonth()}/{new Date(date[0]).getDate()}/
-              {new Date(date[0]).getFullYear()} - {new Date(date[1]).getMonth()}
-              /{new Date(date[1]).getDate()}/{new Date(date[1]).getFullYear()}
-            </Caption>
+            <Caption className="fourteenFont">{city}</Caption>
           </AntCol>
         </AntRow>
-        <AntRow>
-          <AntCol className="mr-point-25">
-            <Caption className="sixteenFont" light>
-              Position:
-            </Caption>
-          </AntCol>
-          <AntCol>
-            <Caption className="sixteenFont">{position}</Caption>
-          </AntCol>
-        </AntRow>
+
+        <span className="twelveFont dragging-card-button">
+          Read Application
+        </span>
 
         {/**
          *
          * View Details Button
          *
          */}
-        <AntRow className="mt-1">
-          <Button shape="round" type="primary" block>
-            <Link to={`/applicants/${id}`}>Details</Link>
-          </Button>
-        </AntRow>
       </TabContainer>
     );
   }
