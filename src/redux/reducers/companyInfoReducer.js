e1d21cc39c6dc093d262968aa56cb192752f07fc
Tejas Maraliga <tejas.maraliga@gmail.com>
2020-12-29 23:11:54 -0500

    Update companyInfo reducer to use ES6 syntax

--- src/redux/reducers/companyInfoReducer.js
+++ src/redux/reducers/companyInfoReducer.js
@@ -13,43 +13,52 @@ const companyInfoReducer = (
 ) => {
   switch (action.type) {
     case "UPDATE_NAME":
-      return Object.assign({}, state, {
-        name: action.name,
-      });
+      return {
+        ...state,
+        name: action.name
+      }
     case "UPDATE_DESCRIPTION":
-      return Object.assign({}, state, {
-        description: action.description,
-      });
+      return {
+        ...state,
+        description: action.description
+      }
     case "UPDATE_WEBSITE":
-      return Object.assign({}, state, {
-        website: action.website,
-      });
+      return {
+        ...state,
+        website: action.website
+      }
     case "UPDATE_EMAIL":
-      return Object.assign({}, state, {
-        email: action.email,
-      });
+      return {
+        ...state,
+        email: action.email
+      }
     case "UPDATE_PHONE_NUMBER":
-      return Object.assign({}, state, {
-        phoneNumber: action.phoneNumber,
-      });
+      return {
+        ...state,
+        phoneNumber: action.phoneNumber
+      }
     case "UPDATE_AVATAR":
-      return Object.assign({}, state, {
-        avatar: action.avatar,
-      });
+      return {
+        ...state,
+        avatar: action.avatar
+      }
     case "UPDATE_ID":
-      return Object.assign({}, state, {
-        id: action.id,
-      });
+      return {
+        ...state,
+        id: action.id
+      }
     case "UPDATE_CANDIDATES":
-      return Object.assign({}, state, {
-        candidates: action.candidates,
-      });
+      return {
+        ...state,
+        candidates: action.candidates
+      }
     case "UPDATE_CANDIDATE_STATUS":
       let newCandidates = state.candidates.slice();
       newCandidates[action.index].status = action.status;
-      return Object.assign({}, state, {
-        candidates: newCandidates,
-      });
+      return {
+        ...state,
+        candidates: newCandidates
+      }
     default:
       return state;
   }
