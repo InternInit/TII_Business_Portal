01d23d2f488ae8816a9144fa1502d3bd0108a9e2
JosephZhang-LDL <20ZhangJ@nsboroschools.net>
2021-01-15 00:19:44 -0500

    Adjusted student feedback tabs with resized read mores

--- src/components/InternFeedback/InternPastFeedback.jsx
+++ src/components/InternFeedback/InternPastFeedback.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Row, Col, Avatar, Button, Grid } from "antd";
 
@@ -15,6 +15,25 @@ const InternPastFeedback = (props) => {
   let { student } = props;
   let readMore = false;
 
+  return (
+    <Row className="mt-1">
+      <Col xs={24} md={10}></Col>
+      <Col xs={24} md={14}>
+        <Header className="twentyTwoFont mb-point-25" bolded>
+          Past Feedback
+        </Header>
+
+        {student.feedback.map((data) => (
+          <FeedbackTab student={student} data={data} />
+        ))}
+      </Col>
+    </Row>
+  );
+};
+
+const FeedbackTab = ({ data, student, feedback }) => {
+  const [readMore, toggleReadMore] = useState(false);
+
   const { useBreakpoint } = Grid;
   const screens = useBreakpoint();
 
@@ -28,142 +47,80 @@ const InternPastFeedback = (props) => {
     .map((breakpoint) => breakpoint[0])
     .includes("sm");
 
-  const isLg = Object.entries(screens)
+  const isMd = Object.entries(screens)
     .filter((screen) => !!screen[1])
     .map((breakpoint) => breakpoint[0])
-    .includes("lg");
+    .includes("md");
 
   return (
-    <Row className="mt-1">
-      <Col xs={24} md={10}>
-      </Col>
-      <Col xs={24} md={14}>
-        <Header className="twentyTwoFont mb-point-25" bolded>
-          Past Feedback
-        </Header>
+    <>
+      <TabContainer className="mb-1 student-intern-tab-container" hoverable>
+        <Row gutter={16} wrap={false}>
+          <Col flex="40px">
+            <Avatar src={student.image} size={40} />
+          </Col>
+          <Col flex="auto">
+            {/* Name & Date Row */}
+            <Row justify="space-between">
+              <Col span={24}>
+                <Header className="twentyFont">
+                  {student.formData[0]["First Name"] +
+                    " " +
+                    student.formData[0]["Last Name"]}
+                </Header>
+              </Col>
 
-        {student.feedback.map((data) => {
-          return (
-            <TabContainer className="mb-1 student-intern-tab-container" hoverable>
-              {/* Avatar Column */}
-              {!isLg ? (
-                <Row gutter={16} wrap={false}>
-                  <Col flex="40px">
-                    <Avatar src={student.image} size={40} />
-                  </Col>
-                  <Col flex="auto">
-                    {/* Name & Date Row */}
-                    <Row justify="space-between">
-                      <Col span={24}>
-                        <Header className="twentyFont">
-                          {student.formData[0]["First Name"] +
-                            " " +
-                            student.formData[0]["Last Name"]}
-                        </Header>
-                      </Col>
-
-                      <Col
-                        //style={{backgroundColor: "red"}}
-                        className="universal-middle"
-                        span={24}
-                      >
-                        <Row justify="start">
-                          <Caption className="twelveFont" light>
-                            <div style={{ padding: "0px 0px 6px 0px" }}>
-                              {moment(data.date).format("MM/DD/YYYY")}
-                            </div>
-                          </Caption>
-                        </Row>
-                      </Col>
-                    </Row>
-
-                    {/* Feedback Row */}
-                    <Row>
-                      <Body className="fourteenFont universal-left">
-                        {!isSm && !isXs ? (
-                          <div>
-                            {(readMore = false)}
-                            {data.comment}
-                          </div>
-                        ) : (
-                          <div
-                            className="intern-dashboard-shortened-feedback"
-                            style={{ height: "80px", overflow: "hidden" }}
-                          >
-                            {(readMore = true)}
-                            {data.comment}
-                          </div>
-                        )}
-                      </Body>
-                    </Row>
-
-                    {/* Read more Button */}
-                    <Row justify="end">
-                      {readMore ? <Button type="link">Read more</Button> : null}
-                    </Row>
-                  </Col>
+              <Col
+                //style={{backgroundColor: "red"}}
+                className="universal-middle"
+                span={24}
+              >
+                <Row justify="start">
+                  <Caption className="twelveFont" light>
+                    <div style={{ padding: "0px 0px 6px 0px" }}>
+                      {moment(data.date).format("MM/DD/YYYY")}
+                    </div>
+                  </Caption>
                 </Row>
-              ) : (
-                <Row gutter={16} wrap={false}>
-                  <Col flex="48px">
-                    <Avatar src={student.image} size={48} />
-                  </Col>{" "}
-                  <Col flex="auto">
-                    {/* Name & Date Row */}
-                    <Row justify="space-between">
-                      <Col>
-                        <Header className="twentyTwoFont">
-                          {student.formData[0]["First Name"] +
-                            " " +
-                            student.formData[0]["Last Name"]}
-                        </Header>
-                      </Col>
+              </Col>
+            </Row>
 
-                      <Col
-                        className="universal-middle"
-                      >
-                        <Row justify="end">
-                        <Caption className="fourteenFont" light>
-                          {moment(data.date).format("MM/DD/YYYY")}
-                        </Caption>
-                        </Row>
-                      </Col>
-                    </Row>
+            {/* Feedback Row */}
+            <Row>
+              <Body className="fourteenFont universal-left">
+                {isMd && data.comment.length > 200 ? (
+                  <div
+                    className="intern-dashboard-shortened-feedback"
+                    style={{ overflow: "hidden", height: "80px" }}
+                  >
+                    {data.comment.length}
+                    {data.comment}
+                  </div>
+                ) : (isSm || isXs) && !isMd && data.comment.length > 100 ? (
+                  <div
+                    className="intern-dashboard-shortened-feedback"
+                    style={{ overflow: "hidden", height: "80px" }}
+                  >
+                    {data.comment}
+                  </div>
+                ) : (
+                  <div>{data.comment}</div>
+                )}
+              </Body>
+            </Row>
 
-                    {/* Feedback Row */}
-                    <Row>
-                      <Body className="fourteenFont universal-left">
-                        {data.comment.length < 200 ? (
-                          <div>
-                            {(readMore = false)}
-                            {data.comment}
-                          </div>
-                        ) : (
-                          <div
-                            className="intern-dashboard-shortened-feedback"
-                            style={{ height: "80px", overflow: "hidden" }}
-                          >
-                            {(readMore = true)}
-                            {data.comment}
-                          </div>
-                        )}
-                      </Body>
-                    </Row>
-
-                    {/* Read more Button */}
-                    <Row justify="end">
-                      {readMore ? <Button type="link">Read more</Button> : null}
-                    </Row>
-                  </Col>
-                </Row>
+            {/* Read more Button */}
+            <Row justify="end">
+              {data.comment.length >= 200 && (
+                <Button type="link" onClick={() => toggleReadMore(!readMore)}>
+                  Read more
+                </Button>
               )}
-
-              {/* Name, Date, Comment Column */}
-            </TabContainer>
-          );
-        })}
-      </Col>
-    </Row>
+            </Row>
+          </Col>
+        </Row>
+      </TabContainer>
+    </>
   );
 };
 
