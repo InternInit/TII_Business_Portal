7f45246fbdfde8e07906af89d60ac0f725e7f2f0
JosephZhang-LDL <20ZhangJ@nsboroschools.net>
2021-01-16 22:17:31 -0500

    Finished basic design on grade display

--- src/components/InternFeedback/InternPastGrades.jsx
+++ src/components/InternFeedback/InternPastGrades.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 
-import { Row as AntRow, Col as AntCol, Pagination } from "antd";
+import { Row as AntRow, Col as AntCol, Pagination, Table } from "antd";
 
 import {
   Header,
-  Body,
   Caption,
   TabContainer,
 } from "../Styled/FundamentalComponents.jsx";
@@ -13,6 +12,7 @@ import _ from "underscore";
 
 import GradeCard from "./GradeCard.jsx";
 
+const { Column, ColumnGroup } = Table;
 const GRADES_PER_PAGE = 1;
 
 const sortReview = (review) => {
@@ -58,12 +58,59 @@ const InternPastGrades = (props) => {
           </AntRow>
         </AntCol>
         <AntCol sm={14} xs={24}>
-          <Header bolded className="twentyTwoFont">
+          <Header bolded className="twentyTwoFont mb-point-25">
             Past Grades
           </Header>
+          <GradeTable
+            grades={_.sortBy(props.student.grades.filter((grade) => grade.isFinished), "dueDate")}
+          />
         </AntCol>
       </AntRow>
     </>
+  );
+};
+
+const GradeTable = (props) => {
+  return (
+    <>
+      <TabContainer style={{height: "467px"}}>
+        <AntRow
+          justify="space-between"
+          className="px-2 py-1 intern-past-grades-col-header"
+        >
+          <AntCol>
+            <Header className="twentyFont">Date Completed</Header>
+          </AntCol>
+          <AntCol>
+            <Header className="twentyFont">Grade</Header>
+          </AntCol>
+        </AntRow>
+        {props.grades.map((grade) => (
+          <GradeRow grade={grade} />
+        ))}
+      </TabContainer>
+    </>
+  );
+};
+
+const GradeRow = (props) => {
+  /**
+   * @TODO
+   * Convert Due Date to Date Completed
+   */
+  return (
+    <AntRow justify="space-between" className="intern-past-grades-grade-row">
+      <AntCol>
+        <Caption className="sixteenFont">
+          {props.grade.dueDateFormatted}
+        </Caption>
+      </AntCol>
+      <AntCol>
+        <Caption className="sixteenFont">
+          {props.grade.assessment ? props.grade.assessment : "A"}
+        </Caption>
+      </AntCol>
+    </AntRow>
   );
 };
 
