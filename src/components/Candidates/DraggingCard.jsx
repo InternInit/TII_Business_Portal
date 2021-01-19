452223c291877e0febf65291d7ae7b0aaa3061df
JosephZhang-LDL <20ZhangJ@nsboroschools.net>
2021-01-18 23:07:12 -0500

    Basic dropdown menu for adding tags

--- src/components/Candidates/DraggingCard.jsx
+++ src/components/Candidates/DraggingCard.jsx
@@ -1,5 +1,14 @@
 import React from "react";
-import { Button, Avatar, Row as AntRow, Col as AntCol, Tooltip } from "antd";
+import {
+  Button,
+  Avatar,
+  Row as AntRow,
+  Col as AntCol,
+  Tooltip,
+  Modal,
+  Dropdown,
+  Menu,
+} from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { Link } from "react-router-dom";
@@ -13,93 +22,116 @@ class DraggingCard extends React.Component {
   render() {
     let { name, date, city, position, avatar, id } = this.props;
     return (
-      <TabContainer className="py-1-5 px-2 responsive-tab-container">
-        {/**
+      <>
+        <TabContainer className="py-1-5 px-2 responsive-tab-container">
+          {/**
          * Temporarily removing this to test a new add tag button
          * 
          * <Caption className="twelveFont dragging-card-date" light right>
           Applied on <br /> {date[0]}
     </Caption>*/}
 
-        {/**
-         *
-         * Row Containing Name + Avatar
-         *
-         */}
-        <AntRow justify="start" align="middle" style={{ position: "relative" }}>
-          <AntCol>
-            <Avatar src={avatar} size={40} icon={<UserOutlined />} />
-          </AntCol>
-          <AntCol offset={1}>
-            <AntRow>
-              <Header className="twentyFont" bolded>
-                {name}
-              </Header>
-            </AntRow>
-            <AntRow>
-              <Caption
-                className="fourteenFont"
-                style={{ marginTop: "-5px" }}
-                light
-                thin
-                left
-              >
-                {position}
-              </Caption>
-            </AntRow>
-          </AntCol>
-          {this.props.status.includes("Interview") ? (
-            <Tooltip title="Categorize Interview">
-              <AiOutlinePlusCircle className="dragging-card-add-tag-icon" />
-            </Tooltip>
-          ) : null}
-        </AntRow>
+          {/**
+           *
+           * Row Containing Name + Avatar
+           *
+           */}
+          <AntRow
+            justify="start"
+            align="middle"
+            style={{ position: "relative" }}
+          >
+            <AntCol>
+              <Avatar src={avatar} size={40} icon={<UserOutlined />} />
+            </AntCol>
+            <AntCol offset={1}>
+              <AntRow>
+                <Header className="twentyFont" bolded>
+                  {name}
+                </Header>
+              </AntRow>
+              <AntRow>
+                <Caption
+                  className="fourteenFont"
+                  style={{ marginTop: "-5px" }}
+                  light
+                  thin
+                  left
+                >
+                  {position}
+                </Caption>
+              </AntRow>
+            </AntCol>
+            {this.props.status.includes("Interview") ? (
+              <Tooltip title="Categorize Interview">
+                <Dropdown
+                  overlay={
+                    <Menu>
+                      <Menu.Item>Online Interview</Menu.Item>
+                      <Menu.Item>Phone Interview</Menu.Item>
+                      <Menu.Item>In-Person Interview</Menu.Item>
+                    </Menu>
+                  }
+                  placement="bottomRight"
+                  arrow
+                  trigger={["click"]}
+                >
+                  <AiOutlinePlusCircle className="dragging-card-add-tag-icon" />
+                </Dropdown>
+              </Tooltip>
+            ) : null}
+          </AntRow>
 
-        <AntRow className="mt-1">
-          <Caption className="fourteenFont mr-point-25" light left>
-            Applied on:
-          </Caption>
-          <AntCol>
-            <Caption className="fourteenFont">{date[0]}</Caption>
-          </AntCol>
-        </AntRow>
-        <AntRow className="mb-1">
-          <AntCol>
-            <Caption className="fourteenFont mr-point-25" light>
-              Location:{" "}
+          <AntRow className="mt-1">
+            <Caption className="fourteenFont mr-point-25" light left>
+              Applied on:
             </Caption>
-          </AntCol>
-          <AntCol>
-            <Caption className="fourteenFont">{city}</Caption>
-          </AntCol>
-        </AntRow>
+            <AntCol>
+              <Caption className="fourteenFont">{date[0]}</Caption>
+            </AntCol>
+          </AntRow>
+          <AntRow className="mb-1">
+            <AntCol>
+              <Caption className="fourteenFont mr-point-25" light>
+                Location:{" "}
+              </Caption>
+            </AntCol>
+            <AntCol>
+              <Caption className="fourteenFont">{city}</Caption>
+            </AntCol>
+          </AntRow>
 
-        <AntRow justify="space-between">
-          <AntCol>
-            <Link to={`/applicants/${this.props.id}`}>
-              <Button type="ghost" shape="round" style={{ marginLeft: "-3px" }}>
-                Details
+          <AntRow justify="space-between">
+            <AntCol>
+              <Link to={`/applicants/${this.props.id}`}>
+                <Button
+                  type="ghost"
+                  shape="round"
+                  style={{ marginLeft: "-3px" }}
+                >
+                  Details
+                </Button>
+              </Link>
+            </AntCol>
+            <AntCol>
+              <Button
+                type="danger"
+                shape="round"
+                style={{ marginRight: "-7px" }}
+                onClick={() => this.props.updateCandidateStatus(id, "Rejected")}
+              >
+                Remove
               </Button>
-            </Link>
-          </AntCol>
-          <AntCol>
-            <Button
-              type="danger"
-              shape="round"
-              style={{ marginRight: "-7px" }}
-              onClick={() => this.props.updateCandidateStatus(id, "Rejected")}
-            >
-              Remove
-            </Button>
-          </AntCol>
-        </AntRow>
+            </AntCol>
+          </AntRow>
 
-        {/**
-         *
-         * View Details Button
-         *
-         */}
-      </TabContainer>
+          {/**
+           *
+           * View Details Button
+           *
+           */}
+        </TabContainer>
+      </>
     );
   }
 }
