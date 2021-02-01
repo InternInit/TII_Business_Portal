75fb7c4d142ae47f0336e30f59ae456fb9bf80a6
JosephZhang-LDL <20ZhangJ@nsboroschools.net>
2021-02-01 12:02:14 -0500

    Finished page transitions

--- src/components/CompanyUsers/Employeepage.jsx
+++ src/components/CompanyUsers/Employeepage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import styled from "styled-components";
 import { Button, Row as AntRow, Col as AntCol } from "antd";
+import { Transition, config } from "react-spring/renderprops";
 import NavSearch from "../General/NavSearch.jsx";
 
 import CompanyAccount from "./CompanyAccount";
@@ -9,7 +9,7 @@ import {
   InnerContainer,
 } from "../Styled/FundamentalComponents.jsx";
 
-import { withRouter, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const ButtonStyle = {
   width: "100%",
@@ -28,24 +28,38 @@ class Employeepage extends Component {
           //@TODO implement functionality
           searchBar={false}
         />
-        <InnerContainer className="py-2">
-          <AntRow gutter={[32, 16]}>
-            <AntCol xs={24} md={8} lg={5}>
-              <Link to="/users/new-account">
-                <Button type="default" style={ButtonStyle}>
-                  <span className="sixteenFont">Add User Account</span>
-                </Button>
-              </Link>
-            </AntCol>
-          </AntRow>
-          {this.props.users.map((user, index) => (
-            <CompanyAccount
-              name={user.name}
-              role={user["custom:role"]}
-              id={user.sub}
-            />
-          ))}
-        </InnerContainer>
+        <Transition
+          items={this.props.location.pathname}
+          from={{ opacity: 0.5, transform: "translateY(20px)" }}
+          enter={{ opacity: 1, transform: "translateY(0px)" }}
+          leave={{ opacity: 1 }}
+          config={config.stiff}
+        >
+          {(location) => (props) => (
+            <InnerContainer
+              key="employeePageContainer"
+              className="py-2"
+              style={{ ...props }}
+            >
+              <AntRow gutter={[32, 16]}>
+                <AntCol xs={24} md={8} lg={5}>
+                  <Link to="/users/new-account">
+                    <Button type="default" style={ButtonStyle}>
+                      <span className="sixteenFont">Add User Account</span>
+                    </Button>
+                  </Link>
+                </AntCol>
+              </AntRow>
+              {this.props.users.map((user, index) => (
+                <CompanyAccount
+                  name={user.name}
+                  role={user["custom:role"]}
+                  id={user.sub}
+                />
+              ))}
+            </InnerContainer>
+          )}
+        </Transition>
       </PageContainer>
     );
   }
