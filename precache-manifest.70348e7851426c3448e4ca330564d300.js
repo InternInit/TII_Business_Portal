92e26c0552d46c6c4aa33472a4c751f822868336
tm1287 <tejas.maraliga@gmail.com>
2022-01-09 22:20:32 -0500

    Updates

---
+++ precache-manifest.70348e7851426c3448e4ca330564d300.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1758f25835b2328d2cd125cbf2fe017",
+    "url": "/index.html"
+  },
+  {
+    "revision": "371af45c551fafe8f297",
+    "url": "/static/css/2.60ba142b.chunk.css"
+  },
+  {
+    "revision": "29afb8a7898777aade4c",
+    "url": "/static/css/main.dbfc9499.chunk.css"
+  },
+  {
+    "revision": "371af45c551fafe8f297",
+    "url": "/static/js/2.b293e79d.chunk.js"
+  },
+  {
+    "revision": "b547c3fdbdcb038620a3283c2fb7f233",
+    "url": "/static/js/2.b293e79d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29afb8a7898777aade4c",
+    "url": "/static/js/main.b53b5af8.chunk.js"
+  },
+  {
+    "revision": "efe4843eaea91536efa7",
+    "url": "/static/js/runtime-main.75c33c3e.js"
+  },
+  {
+    "revision": "87b13b7b14fece3d2f0b7db1d53057de",
+    "url": "/static/media/LoginSignupImage.87b13b7b.svg"
+  }
+]);
